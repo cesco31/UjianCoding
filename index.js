1c90886eea2da837267642f590e9a83f66aa3bb2
Cesco <157945883+cesco31@users.noreply.github.com>
2026-02-06 14:17:24 +0700

    Perubahan Javascript

--- index.js
+++ index.js
@@ -307,8 +307,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 //Kunci Jawaban dan Cek jawaban yang sudah diisi
     window.tampilkanInfo = tampilkanInfo;
-    document.addEventListener("submit", function (e) {
-        e.preventDefault();
+    document.addEventListener("submit", function () {;
         const pilihan = document.getElementById("pilihSoal").value;
         const nama = document.getElementById("name").value;
         const email = document.getElementById("email").value;
@@ -518,4 +517,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }});
 
     })
+
 
